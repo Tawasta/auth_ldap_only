eaca0fe63e0166ea0e9f6fe23b4fd961918e2597
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-06-01 13:12:39 +0300

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Authentication via LDAP Only',
+    'category': 'Authentication',
+    'version': '1.0',
+    'author': 'Oy Tawasta Technologies Ltd',
+    'website': 'http://www.tawasta.fi',
+    'depends': [
+        'auth_ldap',
+    ],
+    'description': 'LDAP authentication that allows ONLY LDAP-logins. No local logins (admin excluded)'
+}
