8eca8bb28e39e2c76ff06441ff582316b6963a92
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-06-02 10:45:35 +0300

    Update the description

--- __openerp__.py
+++ __openerp__.py
@@ -21,5 +21,18 @@
     'depends': [
         'auth_ldap',
     ],
-    'description': 'LDAP authentication that allows ONLY LDAP-logins. No local logins (admin excluded)'
+    'description': 
+'''
+Auth LDAP Only
+--------------
+
+Auth LDAP Only is an extension to [auth_ldap](https://github.com/OCA/OCB/tree/8.0/addons/auth_ldap)
+Its purpose is to disable logins against passwords in Odoo res_users database.
+
+Features
+--------
+
+* Only allows LDAP-authorization (disables local logins)
+* Admin is excluded from LDAP-authorization, and can login via local password 
+'''
 }
