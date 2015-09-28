2a27457f4bc3f98298e5d76e8217a1142483b049
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-28 12:10:06 +0300

    Move models to models dir

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Authentication via LDAP Only',
     'category': 'Authentication',
-    'version': '8.0.0.1.4',
+    'version': '8.0.0.1.5',
     'author': 'Oy Tawasta Technologies Ltd',
     'website': 'http://www.tawasta.fi',
     'depends': [
